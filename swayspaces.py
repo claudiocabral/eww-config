1615466bbd23b5fe83845a469bef8579e1c68d36
Claudio Cabral <cl@udio.co>
2026-05-22 17:39:03 -0300

    swayspaces: properly handle swaymsg events

--- swayspaces.py
+++ swayspaces.py
@@ -2,27 +2,74 @@
 
 import subprocess
 import json
+import sys
 
+data = []
 
 def get_workspaces():
-    output = subprocess.check_output(["swaymsg", "-t", "get_workspaces"])
-    return json.loads(output.decode("utf-8"))
+    output = subprocess.check_output(["swaymsg", "-t", "get_workspaces", "--raw"])
+    data = output.decode("utf-8")
+    return generate_workspace_data(data)
+
+def handle_init(change):
+    if not any(item["name"] == change["current"]["name"] for item in data):
+        data.append({
+            "name": change["current"].get("name", "test"),
+            "output": change["current"].get("output", "best"),
+            "focused": change["current"].get("focused", False),
+        });
+
+def handle_empty(change):
+    for item in data:
+        if item["name"] == change["current"]["name"]:
+            data.remove(item)
+
+def handle_focus(change):
+    for item in data:
+        if ("current" in change
+            and change["current"] is not None
+            and item["name"] == change["current"]["name"]):
+            item["output"] =  change["current"]["output"]
+            item["focused"] = change["current"]["focused"]
+            item["focused"] = (change["current"]["focused"] or
+                               any(node.get("focused") for node in
+                                   change["current"]["nodes"]))
+        if ("old" in change
+            and change["old"] is not None
+            and item["name"] == change["old"].get("name", "")):
+                item["output"] =  change["old"]["output"]
+                item["focused"] = (change["old"]["focused"] or
+                                   any(node.get("focused") for node in
+                                       change["old"]["nodes"]))
+
+def handle_change(change):
+    match change["change"]:
+        case "focus":
+            return handle_focus(change)
+        case "init":
+            return handle_init(change)
+        case "empty":
+            return handle_empty(change)
+        case _:
+            print(change["change"], sys.stderr)
 
 
-def generate_workspace_data() -> dict:
-    data = []
-    for wsp in get_workspaces():
-        if wsp["output"] not in data:
-            data.append(
-            {
-                "name": wsp["name"],
-                "monitor": wsp["output"],
-                "focused": wsp["focused"],
-                "visible": wsp["visible"],
-            }
-        )
-    return data
 
+def parse(line) -> dict:
+    workspaces = json.loads(line)
+    if isinstance(workspaces, dict):
+        return handle_change(workspaces)
+    for wsp in workspaces:
+        data.append({
+            "name": wsp.get("name", "test"),
+            "output": wsp.get("output", "best"),
+            "focused": wsp.get("focused", False),
+        })
+
+def generate_workspace_data(line) -> dict:
+    parse(line)
+    data.sort(key=lambda item: item["name"])
+    return json.dumps(data)
 
 if __name__ == "__main__":
     process = subprocess.Popen(
@@ -32,8 +79,9 @@ if __name__ == "__main__":
     if process.stdout is None:
         print("Error: could not subscribe to sway events")
         exit(1)
+    print(get_workspaces(), flush=True)
     while True:
-        print(json.dumps(generate_workspace_data()), flush=True)
         line = process.stdout.readline().decode("utf-8")
-        if line == "":
-            break
+        #if line == "":
+        #    break
+        print(generate_workspace_data(line), flush=True)
